7a0502d186b79960b53f013f532e4700d9dc64c1
Krzysztof Satola <2679192+ksatola@users.noreply.github.com>
2025-07-17 14:19:51 +0200

    Update install_dependencies.sh

--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -5,33 +5,15 @@ PYTHON_VER=$2
 CPU=$(uname -m) # Get the CPU architecture
 
 
-# Installing prerequisites
-apt-get update && \
-    apt-get install -y \
-    python3-launchpadlib \
-    vim \
-    && apt update 
+# Poetry, Poe the Poet, and Pre-commit
+poetry shell
+poetry run pre-commit install
+poetry self add 'poethepoet[poetry_plugin]'
 
 
-# Install miniconda
-apt update && apt-get install -y --no-install-recommends \
-    software-properties-common \
-    && add-apt-repository -y ppa:deadsnakes/ppa \
-    && apt update 
-
-wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-${CPU}.sh -O ~/miniconda.sh \
-    && /bin/bash ~/miniconda.sh -b -p /opt/conda \
-    && export PATH=/opt/conda/bin:$PATH \
-    && conda init bash \
-    && conda install conda-build
-
-# Set environment
-. /root/.bashrc \
-    && conda create -y --name $CONDA_ENV python=$PYTHON_VER 
-
-echo "conda activate $CONDA_ENV" >> ~/.bashrc
-
-conda activate $CONDA_ENV
-
-# Install the Python packages
-pip3 install -r requirements/requirements.txt
+# AWS CLI
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+yum remove awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
